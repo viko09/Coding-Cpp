c2f180ac042df0d0959f1bb33b70b6053b390089
vikoluna <vmlm2509@gmail.com>
2021-09-19 00:21:37 -0500

    Changes to the code

--- 1. Introducción/datosbasicos.cpp
+++ 1. Introducción/datosbasicos.cpp
@@ -4,14 +4,36 @@
 #include<iostream>
 
 using namespace std;
-int main(){
-    int entero = 25;
-    float flotis = 20.2109;
-    double doble = 0.93932842952489629874310; //este es muy largo
-    char caracter = '@'; //son caracteres y deben ir entre ''
 
-    cout<<entero<<endl;
-    cout<<flotis<<endl;
-    cout<<doble<<endl;
-    cout<<caracter<<endl;
+int main(){
+
+    int entero = 25; // This is a simple integer
+
+    float flotis = 20.2109; // Data with decimal point
+
+    double doble = 0.93932842952489629874310; // This is very large
+
+    char caracter = '@'; // Those characters must be between ''
+
+    bool boli = true; // bool is a boolean data type, it must be True and False
+
+    bool boli2 = false; // bool is a boolean data type, it must be True and False
+
+    // void specifies a empty parameter list and also returns types for a function
+
+    cout<<"\nThis is an integer: "<<entero<<endl;
+    cout<<"This is a float: "<<flotis<<endl;
+    cout<<"This is a double: "<<doble<<endl;
+    cout<<"This is a character: "<<caracter<<endl;
+    cout<<"This is a true boolean: "<<boli<<endl;
+    cout<<"This is a false boolean: "<<boli2<<endl;
+
+    cout<<"\nSize of char: "<<sizeof(char )<<" bytes"<<endl;
+    cout<<"Size of int: "<<sizeof(int )<<" bytes"<<endl;
+    cout<<"Size of short int: "<<sizeof (short int)<<" bytes"<<endl;
+    cout<<"Size of long int: "<<sizeof(long int)<<" bytes"<<endl;
+    cout<<"Size of float: "<<sizeof(float )<<" bytes"<<endl;
+    cout<<"Size of double: "<<sizeof(double )<<" bytes"<<endl;
+    cout<<"Size of boolean: "<<sizeof(bool )<<" bytes\n"<<endl;
+
 }
--- 1. Introducción/holamundo.cpp
+++ 1. Introducción/holamundo.cpp
@@ -3,7 +3,13 @@
 //
 #include<iostream>
 using namespace std;
+
 int main(){
+
     cout<<"Hello World \nby. Viko Luna\n";
     return 0;
 }
+
+/* To compile it, write:
+ * $ g++ filename.cpp -o filename
+ */
